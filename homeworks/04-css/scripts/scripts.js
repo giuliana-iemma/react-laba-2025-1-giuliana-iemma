932b05cfaac235897a958c6efffea89b498eec46
Giuliana Iemma <giuliana.iemma@gmail.com>
2025-03-02 20:50:28 -0300

    Added dark-mode script and mobile version media queries

--- homeworks/04-css/scripts/scripts.js
+++ homeworks/04-css/scripts/scripts.js
@@ -1,13 +1,26 @@
 let toggleButton = document.getElementById('theme-toggle');
 
 toggleButton.addEventListener('click', function () {
-  console.log('');
-
-  // var body = document.body;
+  // Change the body to dark-mode
   document.body.classList.toggle('dark-mode');
 
-  const container = document.getElementById('container');
-  console.log(container);
+  // Find the container
 
+  const container = document.getElementById('container');
   container.classList.toggle('dark-mode');
+
+  // Theme icon
+  const hiddenIcon = document.querySelector('#theme-toggle .theme-toggle__image--hidden');
+  const shownIcon = document.querySelector('#theme-toggle .theme-toggle__image');
+
+  if (shownIcon && hiddenIcon) {
+    // Check if the icons exist.
+    // If they do, reset the className and toggle it to the initial classes
+    /* shownIcon.className = 'theme-toggle__image';  
+    hiddenIcon.className = 'theme-toggle__image--hidden';   */
+
+    // Alternate the classes to show or hide the icon
+    hiddenIcon.classList.toggle('theme-toggle__image--hidden');
+    shownIcon.classList.toggle('theme-toggle__image--hidden');
+  }
 });
