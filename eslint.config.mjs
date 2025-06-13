fddbdbde42bf47b446c11cf26c3485a4a3370713
Giuliana Iemma <giuliana.iemma@gmail.com>
2025-06-13 09:39:49 -0300

    Finished filter

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,3 +3,5 @@ import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [{ languageOptions: { globals: globals.node } }, pluginJs.configs.recommended];
+
+
