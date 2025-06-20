340734c309ead4dc7d54f0b2dbc0e74855fb6c40
Giuliana Iemma <giuliana.iemma@gmail.com>
2025-06-20 14:55:04 -0300

    Created testing structure

--- homeworks/25-testing/src/App.jsx
+++ homeworks/25-testing/src/App.jsx
@@ -3,28 +3,28 @@ import AddTask from './components/AddTask';
 import Task from './components/Task';
 
 function App() {
-   //READ
-  function getSavedTasks () {
-      try{
-        const savedTasks = localStorage.getItem('tasks');
-        return savedTasks ? JSON.parse(savedTasks) : [];
-      } catch (error) {
-        console.error("Couldn't find any tasks: ", error);
-        return [];
-      }
+  //READ
+  function getSavedTasks() {
+    try {
+      const savedTasks = localStorage.getItem('tasks');
+      return savedTasks ? JSON.parse(savedTasks) : [];
+    } catch (error) {
+      console.error("Couldn't find any tasks: ", error);
+      return [];
     }
+  }
 
-    const [tasks, setTasks] = useState(getSavedTasks());
+  const [tasks, setTasks] = useState(getSavedTasks());
 
-    //Get tasks when mounted
-    useEffect(()=> {
-    getSavedTasks()
-    }, []); //Executed at mounting
+  //Get tasks when mounted
+  useEffect(() => {
+    getSavedTasks();
+  }, []); //Executed at mounting
 
-    useEffect(()=> {
-        localStorage.setItem('tasks', JSON.stringify(tasks));
-    }, [tasks]); //Executed when tasks are updated
-        
+  useEffect(() => {
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+  }, [tasks]); //Executed when tasks are updated
+
   //CREATE
   const [title, setTitle] = useState('');
   const [addErrorMessage, setAddErrorMessage] = useState('');
@@ -59,7 +59,7 @@ function App() {
       //Clear the states
       setAddErrorMessage('');
       setTitle('');
-    }  
+    }
   }
 
   //UPDATE
@@ -76,12 +76,11 @@ function App() {
   function updateTask(key, title) {
     // const targetTask = tasks.find((task)=> task.key === key);
     const trimmedTitle = title.trim();
-    const taskExists = tasks.some(
-      (task) => task.title === trimmedTitle && task.key !== key
-    );
+    const taskExists = tasks.some((task) => task.title === trimmedTitle && task.key !== key);
     const isValid = /^[a-zA-Z0-9\s]+$/.test(trimmedTitle);
     const updatedTasks = tasks.map((task) =>
-    task.key === key ? {...task, title: trimmedTitle} : task)
+      task.key === key ? { ...task, title: trimmedTitle } : task
+    );
 
     if (trimmedTitle.length === 0) {
       setEditErrorMessage('Please, write something before pressing Add button');
@@ -106,15 +105,16 @@ function App() {
   }
 
   function deleteTask(key) {
-    console.log(tasks)
-    const updatedTasks = tasks.filter(task => task.key !== key);
+    console.log(tasks);
+    const updatedTasks = tasks.filter((task) => task.key !== key);
     console.log(updatedTasks);
     setTasks(updatedTasks);
   }
 
-  function toggleTaskComplete (key) {    
+  function toggleTaskComplete(key) {
     const updatedTasks = tasks.map((task) =>
-        task.key === key ? {...task, isCompleted: !task.isCompleted} : task);
+      task.key === key ? { ...task, isCompleted: !task.isCompleted } : task
+    );
 
     console.log(updatedTasks);
     setTasks(updatedTasks);
@@ -131,21 +131,25 @@ function App() {
         />
 
         <section className="tasks">
-          {tasks && tasks.length === 0 && <p className="tasks__message">This to-do list is empty</p>}
+          {tasks && tasks.length === 0 && (
+            <p className="tasks__message">This to-do list is empty</p>
+          )}
           {!tasks && <p className="tasks__message">This to-do list is empty</p>}
 
-          { tasks && tasks.map((task) => (
-            <Task 
-              title={task.title} 
-              key={task.key} 
-              editFunction={() => makeEditable(task.key, task.title)}
-              isEditing ={editingKey === task.key} //True if this is the task being edited
-              saveChanges={(updatedText) => updateTask(task.key, updatedText)}
-              errorMessage={editErrorMessage}
-              deleteFunction={() => deleteTask(task.key)}
-              isCompleted = {task.isCompleted}
-              toggleCompleteFunction={() => toggleTaskComplete(task.key)}/>
-          ))}
+          {tasks &&
+            tasks.map((task) => (
+              <Task
+                title={task.title}
+                key={task.key}
+                editFunction={() => makeEditable(task.key, task.title)}
+                isEditing={editingKey === task.key} //True if this is the task being edited
+                saveChanges={(updatedText) => updateTask(task.key, updatedText)}
+                errorMessage={editErrorMessage}
+                deleteFunction={() => deleteTask(task.key)}
+                isCompleted={task.isCompleted}
+                toggleCompleteFunction={() => toggleTaskComplete(task.key)}
+              />
+            ))}
         </section>
       </div>
     </>
